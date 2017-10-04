27e0d9f77bdf80b4687a45f1718bb8e7cc374cf7
user.email <Jenson@LAPTOP-6KETLHQV>
2017-10-04 20:43:18 +0800

    提交代码

---
+++ src/main/java/com/cking/danieleurekaserver/DanielEurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.cking.danieleurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * eureka服务器
+ * @author Jenson
+ * 2017年10月4日 上午10:50:01
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class DanielEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielEurekaServerApplication.class, args);
+	}
+}
